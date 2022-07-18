8fe61f0a037b9db0a2e459605552853eefb28d3a
AnthonyR <arankin789@gmail.com>
2022-07-18 14:24:00 -0400

    added mutations for adding thoughts/friends/reactions | created middleware for Auth

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
+const { authMiddleware } = require('./utils/auth');
+
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 
@@ -11,7 +13,8 @@ const PORT = process.env.PORT || 3001;
 // Create a new Apollo Server and pass in our schema dat
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
